957032428d1bad2030256fdfb21943b37eecc1e7
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-12-27 00:26:00 -0200

    Remover arquivos de testes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var testes = require('./routes/testes');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/', testes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/testes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/testes', function(req, res, next) {
-  res.render('testes', { title: 'Ora ora.. Testando' });
-});
-
-module.exports = router;
