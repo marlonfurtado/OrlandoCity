dbd2cf5918f80078f83a08f35a4b17bf0ad02d78
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-12-28 23:40:33 -0200

    Criar reponsividade 'news'

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 
   $(".thumbnails").owlCarousel({
     // autoplay: 3000,
-    items: 4
+    items: 5
+  });
+
+  var owl = $(".thumbnails").data('owlCarousel');
+
+  $('#btn-news-prev').on('click', function(){
+    owl.prev();
+  });
+  $('#btn-news-next').on('click', function(){
+    owl.next();
   });
 
 });
