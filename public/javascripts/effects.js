ac66a6b33eed404b36a63dbfb42fc1884da9cb26
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-12-30 14:05:59 -0200

    Adicionar efeitos de responsividade

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -1,17 +1,38 @@
 $(document).ready(function(){
 
   $(".thumbnails").owlCarousel({
-    // autoplay: 3000,
     items: 5
   });
 
-  var owl = $(".thumbnails").data('owlCarousel');
+  var owl = $(".thumbnails").data("owlCarousel");
 
-  $('#btn-news-prev').on('click', function(){
+  $("#btn-news-prev").on("click", function(){
     owl.prev();
   });
-  $('#btn-news-next').on('click', function(){
+  $("#btn-news-next").on("click", function(){
     owl.next();
+  });
+
+  $("#page-up").on("click", function(event){
+    $("body").animate({
+      scrollTop:0
+    }, 1000);
+
+    event.preventDefault();
+
+  });
+
+  $("#btn-bars").on("click", function(){
+    $("header").toggleClass("open-menu");
+  });
+
+  $("#menu-mobile-mask, .bar-close").on("click", function(){
+    $("header").removeClass("open-menu");
+  });
+  
+  $('#btn-search').on('click', function() {
+    $('header').toggleClass('open-search');
+    $('#input-search-mobile').focus();
   });
 
 });
